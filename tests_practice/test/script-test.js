697c86801ac0a9f6552672bf3686ebec34cfcda8
SPLINTER\darkw <mykola.horbenko22@gmail.com>
2024-06-28 20:29:48 +0200

    tasks(tests_practice): write tests for sum() fucntion

--- tests_practice/test/script-test.js
+++ tests_practice/test/script-test.js
@@ -1,12 +1,33 @@
 import { assert } from 'chai';
-import {pow} from '../script.js';
+import {pow, sum} from '../script.js';
+
 
 describe('Script', function (){
-    it('Raises the a to the power b', function (){
-        assert.equal(pow(2,3), 8);
+    describe('pow()', function (){
+        it('Raises the a to the power b', function (){
+            assert.equal(pow(2,3), 8);
+        });
+
+        it('Zero remains the same', function (){
+            assert.equal(pow(0,2), 0);
+        });
+
+        it('Returns a number', function (){
+            assert.typeOf(pow(2, 3), "Number");
+        });
     });
 
-    it('Zero remains the same', function (){
-        assert.equal(pow(0,5), 0);
-    })
+    describe('sum()', function (){
+        it('Returns a sum of arguments', function (){
+            assert.equal(sum(2, 3), 5);
+        });
+
+        it('Returns a number', function (){
+            assert.typeOf(sum(2,3), "Number");
+        });
+
+        it('Result should be above 5', function (){
+            assert.isAbove(sum(3,3), 5);
+        });
+    });
 });
