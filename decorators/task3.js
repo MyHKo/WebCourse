60d51a3066589197b122d5fb5bb71114a8c1d3c3
SPLINTER\darkw <mykola.horbenko22@gmail.com>
2024-08-06 17:43:42 +0200

    tasks: solve task 3 for decorators

---
+++ decorators/task3.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function print(x){
+    console.log(x);
+}
+
+function debounce(func, wait){
+    let lastCallTime = 0;
+    return function (){
+        if(new Date().getTime() - lastCallTime > wait && lastCallTime !== 0){
+            return func(...arguments);
+        }
+        lastCallTime = new Date().getTime();
+    }
+}
+
+let a = debounce(print, 1000)
+
+a(2)
+a(2)
+setTimeout(a, 2000,3)
