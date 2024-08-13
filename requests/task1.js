804d3ff32d915279f7dd6f2637f920bb509088f9
SPLINTER\darkw <mykola.horbenko22@gmail.com>
2024-08-13 15:37:36 +0200

    tasks: solve task 1 for requests

--- requests/task1.js
+++ requests/task1.js
@@ -1,7 +1,21 @@
 'use strict'
 
-let response
-fetch('https://api.github.com/users/MyHKo')
-.then(response => response.json())
+let names = ['daewfqewgfewrgwerg', 'iliakan', 'MyHKo', 'bib']
 
-console.log(response)
+function getUsers(names){
+    let output = []
+    for (let name of names){
+        fetch(`https://api.github.com/users/${name}`)
+            .then((result) => {
+                if(result.ok){
+                    output.push(result.json())
+                }
+                else {
+                    output.push(null)
+                }
+            })
+    }
+    return output;
+}
+
+console.log(getUsers(names))
