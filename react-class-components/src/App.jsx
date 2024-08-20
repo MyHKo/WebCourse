a159f7eec4bc84a97bc617ba9620b2ebe68d7108
SPLINTER\darkw <mykola.horbenko22@gmail.com>
2024-08-20 06:54:52 +0200

    react-class-components: add class into greeting component

--- react-class-components/src/App.jsx
+++ react-class-components/src/App.jsx
@@ -2,10 +2,23 @@ import './App.css'
 import React from 'react'
 
 class Greetings extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: props.initialName
+        }
+    }
+
+    clickHandler = () => {
+        this.setState({
+            name: "Me"
+        })
+    }
+
     render() {
          return (
              <h1 className="greeting">
-                 Hello, <i className="emphasize">{this.props.name}</i>, Welcome!
+                 Hello, <i className="emphasize" onClick={this.clickHandler}>{this.state.name}</i>, Welcome!
              </h1>
          )
     }
@@ -18,7 +31,7 @@ class App extends React.Component{
     render() {
         return (<><main>
                 <button onClick={() => {this.setState((prev) => ({show: !prev.show}))}}>Hide me</button>
-            {this.state.show && <Greetings name="Victor"></Greetings>}
+            {this.state.show && <Greetings initialName="Victor"></Greetings>}
                 </main>
             </>
         )
