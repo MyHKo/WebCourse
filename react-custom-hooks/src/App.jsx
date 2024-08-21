fc96547a5ad6a399291df625ca3198c93aed4a31
SPLINTER\darkw <mykola.horbenko22@gmail.com>
2024-08-21 19:40:54 +0200

    react-custom-hooks: do some exercises

--- react-custom-hooks/src/App.jsx
+++ react-custom-hooks/src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {createContext, useContext, useState} from "react";
+
+const contextData ={
+    number: 1,
+    string: "Hello"
+}
+
+function useName(defaultValue){
+    const [name, setName] = useState(defaultValue)
+    function handleNameChange(newName){
+        console.log(newName)
+        if(newName.length > 10){
+            setName(newName)
+        } else {
+            setName("Bib")
+        }
+    }
+    return [name, handleNameChange]
+}
+
+const simpleContext = createContext();
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <simpleContext.Provider value={contextData}>
+    <div className="component-1 component">
+        <SmallerComponent />
+    </div>
+      </simpleContext.Provider>
   )
 }
+
+function SmallerComponent() {
+    const data = useContext(simpleContext);
+    const [name, setName] = useName("Vitya")
+    return (
+        <div className="component">
+            <p>Smaller Component {data.string} {name}</p>
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                setName(e.target.elements.text.value);
+                e.target.elements.text.value = ''
+            }}>
+                <input type="text" name="text"/>
+                <button type="submit">
+                    Change Name
+            </button>
+            </form>
+
+        </div>
+    )
+}
+
 
 export default App
