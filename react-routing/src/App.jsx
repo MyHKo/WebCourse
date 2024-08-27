689590c272393027b643602ada0ebeccfe7a801d
SPLINTER\darkw <mykola.horbenko22@gmail.com>
2024-08-27 13:15:48 +0200

    react-routing:[FEAT] add app component

--- react-routing/src/App.jsx
+++ react-routing/src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from "react";
+import {Facts} from "./facts"
+import {Fact} from"./fact"
+import {fetchAllFacts} from "./fetchAllFacts"
+import {fetchFactById} from "./fetchFactById"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [route, setRoute] = useState({
+    path: "/",
+    data: null,
+    status: "not-started",
+    params: {},
+  });
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const {status, path } = route;
+
+  useEffect(() => {
+    setRoute((r) => ({...r, status:"fetching"}))
+
+    if(path === "/") {
+      fetchAllFacts().then((response) => {
+        setRoute({
+          path: "/",
+          data: response,
+          status: "success",
+          params: {}
+        })
+      })
+    }
+
+    if(path === "/fact") {
+      fetchFactById(route.params.id).then((response) => {
+        setRoute({
+          path: "/facts",
+          data: response,
+          status: "success",
+          params: {id: route.params.id}
+        })
+      })
+    }
+  }, [path])
+
+  const navigate = (id) => {
+    setRoute({
+      data: null,
+      path: "/fact",
+      params: {id},
+      status: "fetching",
+    })
+  }
+
+  if(status === "not-started" || status === "fetching") {
+    return <>Please wait, loading data</>;
+  }
+
+  if(path === "/") {
+    return <Facts facts={route.data} navigate={navigate}/>
+  }
+
+  if(path === "/fact") {
+    return <Fact facts={route.data}/>
+  }
 }
 
 export default App
