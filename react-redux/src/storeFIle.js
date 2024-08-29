1a5136c4a90f7bf9e7f7d72aca79c219b2f11643
SPLINTER\darkw <mykola.horbenko22@gmail.com>
2024-08-29 18:15:48 +0200

    react-redux[FEAT]: change the store file

--- react-redux/src/storeFIle.js
+++ react-redux/src/storeFIle.js
@@ -1,4 +1,10 @@
-import {createStore} from "@reduxjs/toolkit";
-import {reducer} from "./reducers.js";
+import {configureStore} from "@reduxjs/toolkit";
+import counterReducer from "./reduxSlice.js";
 
-const store = createStore(reducer);
+const store = configureStore({
+    reducer: {
+        counter: counterReducer
+    }
+});
+
+export default store;
