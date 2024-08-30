28f53f0dcdb8b5d62e5a8a36296ace06507f0684
SPLINTER\darkw <mykola.horbenko22@gmail.com>
2024-08-30 19:49:36 +0200

    do some practise

--- practice/script.js
+++ practice/script.js
@@ -1,18 +1,3 @@
 'use strict';
 
 
-let controller_1 = new AbortController();
-let controller_2 = new AbortController();
-
-new Promise((resolve, reject) => {
-    setTimeout(resolve, 10000);
-    controller_1.signal.addEventListener('abort', resolve);
-}).then(() => console.log('Aborted 1'))
-
-new Promise((resolve, reject) => {
-    setTimeout(resolve, 10000);
-    controller_2.signal.addEventListener('abort', resolve);
-}).then(() => console.log('Aborted 2'))
-
-setTimeout(() => {controller_1.abort}, 6000);
-setTimeout(() =>{controller_2.abort}, 4000);
