3d62b53394367e3337b89cf974114ff376c369cb
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-01-07 20:46:02 +0100

    React-practice: add useContext

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,24 +1,35 @@
-import {useEffect, useState} from "react";
+import {useEffect, useState, useContext} from "react";
+import React from "react"
+
+const ColorContext = React.createContext();
 
 function App() {
     const [counter, setCounter] = useState(0);
     const [name, setName] = useState("Beep");
     const [inputValue, setInputValue] = useState("");
+    const [color, setColor] = useState("green");
+
 
     useEffect(() => {
      console.log("counter", counter);
      return () => {
-         console.log("Clearing everything up")
+         console.log("clearing up");
      };
-    }, [name]);
+    }, [counter]);
 
   return (
       <>
           <div>{name} {counter}</div>
           <br/>
-          <button onClick={() => setCounter((prevState) => {
-              return prevState + 1
-          })}>Add One
+          <button onClick={() => {
+              setCounter((prevState) => {
+                return prevState + 1
+              })
+              if(color === "green")
+                  setColor("blue")
+              else
+                  setColor("green")
+          }}>Add One
           </button>
           <br/>
           <input type={"text"} value={inputValue} onChange={(event) => {
@@ -29,9 +40,19 @@ function App() {
               setName(inputValue)
           }}>Enter
           </button>
+
+          <ColorContext.Provider value={color}>
+              <Child />
+          </ColorContext.Provider>
       </>
   )
 }
 
+function Child() {
+    const color = useContext(ColorContext)
+    return (
+        <h1 style={{ color }}>Child Header</h1>
+    )
+}
 
 export default App
