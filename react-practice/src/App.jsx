de1b253c13a3cfbd4d9e795b5b67d54bbbc212ce
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-01-07 20:10:16 +0100

    React-practice: add input field and more states

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,15 +1,35 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
     const [counter, setCounter] = useState(0);
+    const [name, setName] = useState("Beep");
+    const [inputValue, setInputValue] = useState("");
+
+    useEffect(() => {
+     console.log("counter", counter);
+     return () => {
+         console.log("Clearing everything up")
+     };
+    }, []);
 
   return (
-    <>
-        {counter}<br/>
-        <button onClick={() => setCounter((prevState) => {
-            return prevState + 1
-        })}>Add One</button>
-    </>
+      <>
+          <div>{name} {counter}</div>
+          <br/>
+          <button onClick={() => setCounter((prevState) => {
+              return prevState + 1
+          })}>Add One
+          </button>
+          <br/>
+          <input type={"text"} value={inputValue} onChange={(event) => {
+              setInputValue(event.target.value)
+          }}/>
+          <br/>
+          <button onClick={() => {
+              setName(inputValue)
+          }}>Enter
+          </button>
+      </>
   )
 }
 
