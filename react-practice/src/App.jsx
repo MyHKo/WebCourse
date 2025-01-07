91a89c625c650e9b131cca0ad8b0c3416739936a
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-01-07 19:51:39 +0100

    React-practice: clear redudnant code

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,34 +1,8 @@
-
-function Header(){
-    return (
-        <h1>This is a header</h1>
-    )
-}
-
-function MainText(){
-    return (
-        <p>This is a main text</p>
-    )
-}
-
-function Content(){
-    return (
-        <div>
-            <Header />
-            <MainText />
-        </div>
-    )
-}
-
 function App() {
-
-    console.log(Header())
-    console.log(Content())
 
   return (
     <>
       <div>
-          <Content />
       </div>
     </>
   )
