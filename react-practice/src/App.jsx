282fa86a618fbd35ca1ab06162ea39ec5b9f04a0
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-01-07 19:53:20 +0100

    React-practice: add use state

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,9 +1,14 @@
+import {useState} from "react";
+
 function App() {
+    const [counter, setCounter] = useState(0);
 
   return (
     <>
-      <div>
-      </div>
+        {counter}<br/>
+        <button onClick={() => setCounter((prevState) => {
+            return prevState + 1
+        })}>Add One</button>
     </>
   )
 }
