6bd4e3fc2a1f2cac1e53aac5d4eda44273cb2b60
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-01-13 20:52:58 +0100

    React-practice: add input to the child component

--- react-practice/src/Child.jsx
+++ react-practice/src/Child.jsx
@@ -1,10 +1,20 @@
-import React, {useContext} from "react";
+import React, {useContext, useRef} from "react";
 import {ColorContext} from "./App.jsx";
+
+const ChildInput = (props) => {
+    return (
+        <input />
+    )
+}
 
 function Child() {
     const {colorState} = useContext(ColorContext)
+    const nestedInputRef = useRef(null)
     return (
+        <>
+        <ChildInput ref={nestedInputRef}/>
         <h1 style={{color: colorState.color}}>Child Header</h1>
+        </>
     )
 }
 
