3698b3137d680c4c348d380093837005984af3b2
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-01-14 15:59:43 +0100

    React-practice: add function for resseting index

--- react-practice/src/useState.js
+++ react-practice/src/useState.js
@@ -23,14 +23,22 @@ const ReactX = () => {
     }
 }
 
+const resetIndex = () => {
+    index = 0;
+}
+
 const { useState } = ReactX();
 
 const Component = () => {
     const [state, setState] = useState(1)
+    const [name, setName] = useState("Bob")
 
     console.log(state)
+    console.log(name)
     setState(2)
+    setName("John")
 }
 
 Component()
+resetIndex()
 Component()
