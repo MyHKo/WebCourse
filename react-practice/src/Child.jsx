2ba9f36fe8d503d7892c25810e2717c9b1c37814
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-01-14 15:20:39 +0100

    React-practice: add useImparetiveHandle hook

--- react-practice/src/Child.jsx
+++ react-practice/src/Child.jsx
@@ -1,11 +1,24 @@
-import React, {useContext, useRef} from "react";
+import React, {useContext, useImperativeHandle, useRef} from "react";
 import {ColorContext} from "./App.jsx";
 
-const ChildInput = (props) => {
+const ChildInput = React.forwardRef((props, forwardedRef) => {
+    const localInputRef = useRef(null);
+
+    useImperativeHandle(forwardedRef, () => {
+        return {
+            focusAndBlur: () => {
+                localInputRef.current.focus()
+                setTimeout(() => {
+                    localInputRef.current.blur()
+                }, 1000)
+            }
+        }
+    })
+
     return (
-        <input />
+        <input ref={localInputRef} />
     )
-}
+})
 
 function Child() {
     const {colorState} = useContext(ColorContext)
