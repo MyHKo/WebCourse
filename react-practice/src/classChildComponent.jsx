9764ba3aaff255de71d4f118305b0bc3749d3085
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-01-16 16:18:06 +0100

    React-practice: add a state and state changer to class component

--- react-practice/src/classChildComponent.jsx
+++ react-practice/src/classChildComponent.jsx
@@ -3,6 +3,9 @@ import {Component} from "react"
 class ClassChildComponent extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            counter: 0
+        }
     }
 
     render(){
@@ -10,7 +13,14 @@ class ClassChildComponent extends Component {
 
         return (
             <>
-                <h1>This is a class Child</h1>
+                <h1>This is a class Child</h1><br />
+                <p>This is a child counter state: {this.state.counter}</p><br />
+                <button onClick={() => {
+                    this.setState(
+                        {...this.state,
+                            counter: this.state.counter + 1}
+                    )}}
+                >+</button>
             </>
         )
     }
