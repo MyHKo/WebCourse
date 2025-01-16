959f95f023bfcda53ecd15bb82d3fd9bc6e7147d
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-01-16 16:14:51 +0100

    React-practice: create a class component

---
+++ react-practice/src/classChildComponent.jsx
@@ -0,0 +1,17 @@
+import {Component} from "react"
+
+class ClassChildComponent extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render(){
+
+
+        return (
+            <>
+                <h1>This is a class Child</h1>
+            </>
+        )
+    }
+}
