daa499567ec01fe19f9a45cd0a304a18cbcd1abb
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-04-16 09:42:58 +0200

    Change builder to be an actual class

--- builder.js
+++ builder.js
@@ -1,4 +1,4 @@
-class burger {
+class Burger {
     sauce = false
     lettuce = false
     cheese = false
@@ -16,13 +16,23 @@ class burger {
     }
 }
 
-const burgerBuilder = {
-    burger: {},
-    addSauce: () => { this.burger.sauce = true },
-    addLettuce: () => { this.burger.lettuce = true },
-    addTomato: () => { this.burger.tomato = true },
-    addCheese: () => { this.burger.cheese = true },
-    addPatties: (number) => { this.burger.patties = number },
-    addBun: (number) => { this.burger.buns = number },
-    getBurger: () => { return this.burger },
+class burgerBuilder {
+    burger = {
+        sauce: false,
+        lettuce: false,
+        cheese: false,
+        tomato: false,
+        patties: 1,
+        buns: 2,
+    }
+
+    addSauce = () => { this.burger.sauce = true }
+    addLettuce = () => { this.burger.lettuce = true }
+    addTomato = () => { this.burger.tomato = true }
+    addCheese = () => { this.burger.cheese = true }
+    addPatties = (number) => { this.burger.patties = number }
+    addBun = (number) => { this.burger.buns = number }
+    getBurger = () => { return this.burger }
+    resetBurger = () => { this.burger.burger = {} }
 }
+
